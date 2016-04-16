afca8c117932080e687337c1e289a6769d747251
Ubuntu <ubuntu@ip-10-10-0-48.eu-west-1.compute.internal>
2016-04-16 09:20:31 +0000

    AWS region dded as command line option

--- scripts/kraken_download_db.pl
+++ scripts/kraken_download_db.pl
@@ -8,22 +8,23 @@ use strict;
 
 $ENV{PATH} = "$ENV{PATH}:/vol/scripts:/vol/krona/bin";
 
-my ($krakendb, $krakendb_dir, $download_dir);
+my ($krakendb, $aws_region, $krakendb_dir, $download_dir);
 
 
 GetOptions("krakendb=s"     => \$krakendb,
+	   "aws_region=s"   => \$aws_region,
 	   "download_dir=s" => \$download_dir,
 	   "dest=s"         => \$krakendb_dir
     );
 
-unless ($krakendb && $krakendb_dir && $download_dir) {
-    die "\nusage: $0 -krakendb KRAKEN_DB_S3_URL -download_dir TMP_DOWNLOAD_DIR -dest DESTINATION_DIR
+unless ($krakendb && $aws_region && $krakendb_dir && $download_dir) {
+    die "\nusage: $0 -krakendb KRAKEN_DB_S3_URL -aws_region AWS_REGION -download_dir TMP_DOWNLOAD_DIR -dest DESTINATION_DIR
 
 Small example (4GB Kraken DB): 
-$0 -krakendb s3://bibicloud-demo/kraken-db/minikraken_20140330.tar -download_dir /vol/scratch -dest /vol/mem/krakendb
+$0 -krakendb s3://bibicloud-demo/kraken-db/minikraken_20140330.tar -aws_region eu-west-1 -download_dir /vol/scratch -dest /vol/mem/krakendb
 
 Big example (80GB Kraken DB):
-$0 -krakendb s3://bibicloud-demo/kraken-db/kraken_standard.tar -download_dir /vol/scratch -dest /vol/mem/krakendb
+$0 -krakendb s3://bibicloud-demo/kraken-db/kraken_standard.tar -aws_region eu-west-1 -download_dir /vol/scratch -dest /vol/mem/krakendb
 ";
 }
 
@@ -36,8 +37,8 @@ print STDERR "Downloading Database to $download_dir...\n";
 #print STDERR "/vol/scripts/download.pl -type file -source $krakendb -dest $download_dir\n";
 #system("/vol/scripts/download.pl -type file -source $krakendb -dest $download_dir");
 
-print STDERR "java -jar /vol/scripts/bibis3-1.6.1.jar --region=eu-west-1 -d $krakendb $download_dir/\n";
-system("java -jar /vol/scripts/bibis3-1.6.1.jar --region=eu-west-1 -d $krakendb $download_dir/");
+print STDERR "java -jar /vol/scripts/bibis3-1.6.1.jar --region=$aws_region -d $krakendb $download_dir/\n";
+system("java -jar /vol/scripts/bibis3-1.6.1.jar --region=$aws_region -d $krakendb $download_dir/");
 
 chdir $krakendb_dir;
 my $kraken_tarfile = basename($krakendb);
--- scripts/submit_kraken_download.sh
+++ scripts/submit_kraken_download.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ $# -ne 7 ]
+if [ $# -ne 8 ]
   then
-    echo "Usage: $0 SLOTS NUM_NODES SCRATCHDIR SPOOLDIR MEMDISK KRAKENDB KRAKENDB_DIR"
+    echo "Usage: $0 SLOTS NUM_NODES SCRATCHDIR SPOOLDIR MEMDISK KRAKENDB AWS_REGION KRAKENDB_DIR"
     echo
-    echo "Example: $0 8 2 /vol/scratch /vol/spool /dev/shm s3://bibicloud-demo/kraken-db/minikraken_20140330.tar /vol/mem/krakendb"
+    echo "Example: $0 8 2 /vol/scratch /vol/spool /dev/shm s3://bibicloud-demo/kraken-db/minikraken_20140330.tar eu-west-1 /vol/mem/krakendb"
     exit 1
 fi
 
@@ -14,11 +14,12 @@ SCRATCHDIR=$3
 SPOOLDIR=$4
 MEMDISK=$5
 KRAKENDB=$6
-KRAKENDB_DIR=$7
+AWS_REGION=$7
+KRAKENDB_DIR=$8
 
 PIPELINEHOME=`dirname $0`
 
 echo "Submitting job to SGE and waiting until finished..."
-echo qsub -cwd -t 1-$NUM_NODES -pe multislot $SLOTS $PIPELINEHOME/docker_run.sh $SCRATCHDIR $SPOOLDIR $MEMDISK "/vol/scripts/kraken_download_db.pl -krakendb $KRAKENDB -download_dir $SCRATCHDIR -dest $KRAKENDB_DIR"
-qsub -cwd -N KrakenDB_download -t 1-$NUM_NODES -pe multislot $SLOTS $PIPELINEHOME/docker_run.sh $SCRATCHDIR $SPOOLDIR $MEMDISK "/vol/scripts/kraken_download_db.pl -krakendb $KRAKENDB -download_dir $SCRATCHDIR -dest $KRAKENDB_DIR"
+echo qsub -cwd -t 1-$NUM_NODES -pe multislot $SLOTS $PIPELINEHOME/docker_run.sh $SCRATCHDIR $SPOOLDIR $MEMDISK "/vol/scripts/kraken_download_db.pl -krakendb $KRAKENDB -aws_region $AWS_REGION -download_dir $SCRATCHDIR -dest $KRAKENDB_DIR"
+qsub -cwd -N KrakenDB_download -t 1-$NUM_NODES -pe multislot $SLOTS $PIPELINEHOME/docker_run.sh $SCRATCHDIR $SPOOLDIR $MEMDISK "/vol/scripts/kraken_download_db.pl -krakendb $KRAKENDB -aws_region $AWS_REGION -download_dir $SCRATCHDIR -dest $KRAKENDB_DIR"
 
