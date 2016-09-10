c058b4691a76f218dcd3833d280cc394ad9e2555
Ubuntu <ubuntu@host-192-168-10-18.openstacklocal>
2016-09-10 16:45:01 +0000

    use /vol/scratch in container as database dir

--- container_scripts/kraken_download_db.sh
+++ container_scripts/kraken_download_db.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-  then
-    echo 
-    echo "Usage: $0 SCRATCHDIR"
-    echo 
-    echo "script to download the Mini-Kraken database to SCRATCHDIR"
-    echo 
-    exit 0;
-fi
-
-SCRATCHDIR=$1
+# script to download the Mini-Kraken database to container directory /vol/scratch"
 
 echo "Start downloading database..."
 
 swift -U gcb:swift -K ssbBisjNkXmwgSXbvyAN6CtQJJcW2moMHEAdQVN0 -A http://swift:7480/auth \
-download gcb minikraken.tgz --output $SCRATCHDIR/minikraken.tgz
+download gcb minikraken.tgz --output /vol/scratch/minikraken.tgz
 
 echo "done downloading database."
 
 echo "extracting database files..."
 
-cd $SCRATCHDIR
-tar xvzf $SCRATCHDIR
+cd /vol/scratch
+tar xvzf minikraken.tgz
 
-echo "done extracting files.
+echo "done extracting files."
